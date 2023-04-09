6e8e80c18b1e4141a443ffb9374a125b13b54bd8
Zylleon <56230821+Zylleon@users.noreply.github.com>
2023-04-09 14:20:01 -0600

    wild child backstories

---
+++ Source/ZCB/RecordWorker_TimeAsWildMan.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ZCB
+{
+	public class RecordWorker_TimeAsWildMan : RecordWorker
+	{
+		public override bool ShouldMeasureTimeNow(Pawn pawn)
+		{
+			return pawn.IsWildMan();
+		}
+	}
+
+}
